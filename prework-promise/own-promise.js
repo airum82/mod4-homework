3d49ecf3e7d6090c240b7e6b80974eb650a7dd0a
Arram Mandel <airum82@gmail.com>
2018-08-13 17:59:43 -0600

    Revised promise function

--- prework-promise/own-promise.js
+++ prework-promise/own-promise.js
@@ -1,10 +1,13 @@
 let authUser = 'jim';
 
-const emptyPromise = new Promise((resolve, reject) => {
-  if(authUser) {
-    return resolve(authUser)
-  } else {
-    return reject(Error('not a user'))
-  }
-});
+const checkUser = () => {
+  const emptyPromise = new Promise((resolve, reject) => {
+    if(authUser) {
+      return resolve(authUser)
+    } else {
+      return reject(Error('not a user'))
+    }
+  });
+  return emptyPromise;
+}
 
