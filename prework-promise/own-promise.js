c7dce1095e72eb3f7d9c1b606c1f469d73dc0c8b
Arram Mandel <airum82@gmail.com>
2018-08-14 08:17:07 -0600

    Remade promise functions to match assignment

--- prework-promise/own-promise.js
+++ prework-promise/own-promise.js
@@ -1,22 +1,27 @@
-let authUser = 'jim';
-
-const checkUser = () => {
+const testNum = num => {
   return new Promise((resolve, reject) => {
-    if(authUser) {
-      return resolve(authUser)
+    if(num > 10) {
+      return resolve(num + 'is greater than 10, sucess');
     } else {
-      return reject(Error('not a user'))
-    }
-  });
-}
-
-const checkPw = (pw) => {
-  return new Promise((resolve, reject) => {
-    if (pw === 'sesame') {
-      return resolve('You may enter')
-    } else {
-      return reject(Error('Away with you'))
+      return reject(Error(num + 'is not greater than 10, error'))
     }
   })
 }
+
+const makeAllCaps = words => {
+  return new Promise((resolve, reject) => {
+    return resolve(words.map(word => word.toUpperCase()))
+  })
+}
+
+const sortWords = words => {
+  return new Promise((resolve, reject) => {
+    const checkedForString = words.filter(word => typeof (word) !== 'string')
+    if(checkedForString.length) {
+      return reject(Error('Array contains an element that is not a string'))
+    }
+    return resolve(words.sort())
+  })
+}
+
 
